e58421fe00381b9258571ffebab16ab236695f17
nitinkumar388 <nitin.kumar.98886@gmail.com>
2021-07-13 09:21:20 +0530

    django social web

---
+++ App_Posts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from App_Posts import views
+
+app_name = "App_Posts"
+
+urlpatterns = [
+    path("", views.home, name='home'),
+    path("liked/<pk>/", views.liked, name='liked'),
+    path("unliked/<pk>/", views.unliked, name='unliked'),
+
+]
